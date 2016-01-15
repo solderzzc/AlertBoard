1aa4d64a57fb6eb92966ccb034657bc0f12e6309
Шатров Сергей Владимирович < @md.mash-dvor.ru>
2016-01-15 14:36:01 +0500

    Initial commit

---
+++ app/src/androidTest/java/ru/sersh_perm/trafficlight/ApplicationTest.java
@@ -0,0 +1,13 @@
+package ru.sersh_perm.trafficlight;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
